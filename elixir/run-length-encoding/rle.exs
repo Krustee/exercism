7e0a5bcb221440206a2a8f9bc3ea9103228a8309
Lars Jensen <lcj@trifork.com>
2016-07-08 18:00:53 +0200

    Solved elixir run-length encoding

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -8,11 +8,33 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t) :: String.t
   def encode(string) do
-
+    do_encode(string)
   end
 
+  defp do_encode(<< >>), do: ""
+  defp do_encode(<< c::utf8, rest::binary >>) do
+    do_encode(rest, %{:current_run => c, :count => 1, :output => ""})
+  end 
+  defp do_encode(<< c::utf8, rest::binary >>, %{:current_run => current, :count => count, :output => output} = acc) do
+    if current === c do
+      do_encode(rest, Map.put(acc, :count, acc.count + 1))
+    else
+      do_encode(rest, %{:current_run => c, :count => 1, :output => output <> to_string(count) <> utf_char_to_string(current)})
+    end
+  end
+  defp do_encode("", %{count: count, current_run: current, output: output}) do
+    output <> to_string(count) <> utf_char_to_string(current)
+  end
+  
+  defp utf_char_to_string(value) do
+    to_string([value])
+  end
+  
   @spec decode(String.t) :: String.t
   def decode(string) do
-
+    Regex.scan(~r{(\d+)(\D+)}u, string)
+    |> Enum.reduce("", fn([_matched, count, letter], accStr) -> 
+        accStr <> String.duplicate(letter, String.to_integer(count))
+      end)
   end
 end
